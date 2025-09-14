b3692c36b83dad83ef1e996a810b13a042a9ad15
Roman Grebennikov <grv@dfdx.me>
2025-09-14 16:30:42 +0000

    fix tests

--- tests/core/test_submission.py
+++ tests/core/test_submission.py
@@ -59,5 +59,5 @@ def test_submission():
 
 
 def test_recall():
-    r1 = ExperimentMetrics.recall_score([1, 2, 3, 4], [1, 2, 4, 5])
+    r1 = ExperimentMetrics.recall_score([1, 2, 3, 4], [1, 2, 4, 5], 4)
     assert r1 == 0.75
