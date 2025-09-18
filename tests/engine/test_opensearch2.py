516f2b51dc9107120a93482a49c6035e2687c112
Roman Grebennikov <grv@dfdx.me>
2025-09-18 20:07:29 +0000

    add tests to quants

--- tests/engine/test_opensearch2.py
+++ tests/engine/test_opensearch2.py
@@ -1,32 +1,15 @@
 from hseb.core.config import (
-    Config,
-    ExperimentConfig,
-    DatasetConfig,
-    IndexArgsMatrix,
     QuantDatatype,
-    SearchArgsMatrix,
 )
 
-from tests.engine.base import EngineSuite
+from tests.engine.base import EngineParams, EngineSuite
 
 
 class TestOpensearch2Engine(EngineSuite):
-    def config(self) -> Config:
-        return Config(
+    def params(self):
+        return EngineParams(
             engine="hseb.engine.opensearch.OpenSearchEngine",
             image="opensearchproject/opensearch:2.19.2",
-            dataset=DatasetConfig(
-                dim=384,
-                name="hseb-benchmark/msmarco",
-                query="query-all-MiniLM-L6-v2-1K",
-                corpus="corpus-all-MiniLM-L6-v2-1K",
-            ),
-            experiments=[
-                ExperimentConfig(
-                    tag="test",
-                    k=10,
-                    index=IndexArgsMatrix(m=[16], ef_construction=[64], quant=[QuantDatatype.FLOAT32]),
-                    search=SearchArgsMatrix(ef_search=[16], filter_selectivity=[10, 100]),
-                )
-            ],
+            quantizations=[QuantDatatype.INT8, QuantDatatype.FLOAT16],
+            index_kwargs={"engine": ["lucene", "faiss"]},
         )
